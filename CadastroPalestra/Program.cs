2c1b99496fcd5d01cfa26883011431efd81545a8
Eduardo Kropniczki <dudukrop@gmail.com>
2024-04-21 18:50:12 -0300

    Changed to Prod

--- CadastroPalestra/Program.cs
+++ CadastroPalestra/Program.cs
@@ -18,11 +18,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
